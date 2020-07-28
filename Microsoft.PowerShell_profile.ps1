9c87efce6f617a3cbfaaba75e1b4586063e0bd75
ttay <ttay@umich.edu>
2020-07-28 22:39:50 +0800

    added Get-Youtube

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -57,6 +57,59 @@ function fortune {
     & $fortuneLocation $randNum
 }
 
+function Get-Youtube {
+    [CmdletBinding(SupportsShouldProcess = $True)]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string]$URL,
+        [Switch]$audio,
+        [Switch]$ignoreErrors
+    )
+    $command = $URL, '-o', '%(title)s.%(ext)s'
+    if ($audio) {
+        $command += "-x", "--postprocessor-args", "`"-threads 2`""
+    }
+    if ($ignoreErrors) {
+        $command += "--ignore-errors"
+    }
+    Write-Information "Executing Command..."
+    Write-Information ("youtube-dl " + ($command -join " "))
+    Start-Process -NoNewWindow -Wait -FilePath "youtube-dl" -ArgumentList $command 
+
+    <#
+    .SYNOPSIS
+    Downloads a file using youtube-dl.
+
+    .DESCRIPTION
+    Takes as input a URL, and a switch to download as MP3
+    Uses youtube-dl (a Python extension) to download the files
+
+    .PARAMETER URL
+    Specifies the URL. Mandatory.
+
+    .PARAMETER audio
+    Switch, specifying if the video should be downloaded as an audio file
+    Note that this does not necessarily download an mp3 file, it might
+    download an .opus file
+
+    .PARAMETER ignoreErrors
+    Switch, specifying whether ytdl should Continue on download errors,
+    for example to skip unavailable videos in a playlist
+
+    .LINK
+    https://github.com/ytdl-org/youtube-dl
+
+    .EXAMPLE
+    PS> ./ytdl-wrapper -URL https://www.youtube.com/watch?v=yDOtENyOxyA -audio
+
+    .EXAMPLE
+    PS> ./ytdl-wrapper https://www.youtube.com/playlist?list=PLD6GNPaln5zKG4XKdMCN5YrgkIivJnkYd -ignoreErrors
+
+    .EXAMPLE
+    PS> ./ytdl-wrapper -URL https://www.youtube.com/watch?v=yDOtENyOxyA -WhatIf
+    #>
+}
+
 ######## Aliases ###############
 Set-Alias -Name v -Value nvim-qt
 Set-Alias -Name ssa -Value ssh-agent
@@ -66,5 +119,4 @@ function nimc { nim c $args }
 function nimrun { nim c -r $args }
 
 # On Startup, do:
-# Clear-Host
 fortune

1d621ea8bb13445ca4afad448fce5ce48835a185
ttay <ttay@umich.edu>
2021-06-05 13:06:48 -0700

    added download-github function2

--- .config/fish/functions/download-github.fish
+++ .config/fish/functions/download-github.fish
@@ -1,6 +1,7 @@
-function download-github
-set repoPart (url-parser --part=path $argv[1] | awk -F'/' '{print $2 "/" $3}')
-set tmpFileName (mktemp)
-curl -H "Accept: application/vnd.github.v3+json" -L -o tmpFileName "https://api.github.com/repos/$repoPart/tarball"
-tar -xzvf tmpFileName
+# Defined in /tmp/fish.9jK7Fs/download-github.fish @ line 2
+function download-github --description 'Downloads a repo from github'
+    set repoPart (url-parser --part=path $argv[1] | awk -F'/' '{print $2 "/" $3}')
+    set tmpFileName (mktemp)
+    curl -H "Accept: application/vnd.github.v3+json" -L -o $tmpFileName "https://api.github.com/repos/$repoPart/tarball"
+    tar -xzvf $tmpFileName
 end
